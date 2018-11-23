c02751f763bcd278359bdc14ac3d016160487079
LarsAhman <l.ahman@hotmail.com>
2018-11-24 00:58:29 +0200

    piirto

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-function login(form){
-    if(form.username.value =="admin"){
-        if(form.password.value="admin"){
-            location ="map.html"
-        }else{
-            alert("wrong password")
-        }
-    }else{
-        alert("wrong username")
-    }
-}
+import 'ol/ol.css';
+import {Map, View, Draw, {Tile as TileLayer, Vector as VectorLayer}, {OSM, Vector as VectorSource}} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 0
+  })
+});
