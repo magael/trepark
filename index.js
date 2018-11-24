ebc27ef51805046c577a622eeb6027347d217526
Mikael Jaakkola <mikael.jaakkola@helsinki.fi>
2018-11-25 00:40:12 +0200

    karttaa vähän pienemmäks, testikäyttäjä ja parkingevent, par- ja leave-funktioihin lisää toiminnallisuutta(ne pitää viel sitoo buttoneihin)

--- index.js
+++ index.js
@@ -91,37 +91,37 @@ function initMap() {
     initialize(element, map, infowindow);
   });
   var locationWindow = new google.maps.InfoWindow();
-    function CenterControl(controlDiv, map) {
-      var controlUI = document.createElement('div');
-      controlUI.style.backgroundColor = '#fff';
-      controlUI.style.border = '2px solid #fff';
-      controlUI.style.borderRadius = '3px';
-      controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-      controlUI.style.cursor = 'pointer';
-      controlUI.style.marginBottom = '22px';
-      controlUI.style.textAlign = 'center';
-      controlUI.title = 'Click to recenter the map';
-      controlDiv.appendChild(controlUI);
+  function CenterControl(controlDiv, map) {
+    var controlUI = document.createElement('div');
+    controlUI.style.backgroundColor = '#fff';
+    controlUI.style.border = '2px solid #fff';
+    controlUI.style.borderRadius = '3px';
+    controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.marginBottom = '22px';
+    controlUI.style.textAlign = 'center';
+    controlUI.title = 'Click to recenter the map';
+    controlDiv.appendChild(controlUI);
 
-      var controlText = document.createElement('div');
-      controlText.style.color = 'rgb(25,25,25)';
-      controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-      controlText.style.fontSize = '16px';
-      controlText.style.lineHeight = '38px';
-      controlText.style.paddingLeft = '5px';
-      controlText.style.paddingRight = '5px';
-      controlText.innerHTML = 'Locate Me';
-      controlUI.appendChild(controlText);
+    var controlText = document.createElement('div');
+    controlText.style.color = 'rgb(25,25,25)';
+    controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+    controlText.style.fontSize = '16px';
+    controlText.style.lineHeight = '38px';
+    controlText.style.paddingLeft = '5px';
+    controlText.style.paddingRight = '5px';
+    controlText.innerHTML = 'Locate Me';
+    controlUI.appendChild(controlText);
 
-      controlUI.addEventListener('click', function() {
-          var pos = locate(locationWindow, map, parkingAreas);
-      });
-    }
+    controlUI.addEventListener('click', function () {
+      var pos = locate(locationWindow, map, parkingAreas);
+    });
+  }
 
-var centerControlDiv = document.createElement('div');
-var centerControl = new CenterControl(centerControlDiv, map);
-centerControlDiv.index = 1;
-map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
+  var centerControlDiv = document.createElement('div');
+  var centerControl = new CenterControl(centerControlDiv, map);
+  centerControlDiv.index = 1;
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
 
   var centerControlDiv = document.createElement('div');
   var centerControl = new CenterControl(centerControlDiv, map);
@@ -134,13 +134,13 @@ map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
     lng: 0
   }
   var d = new Date();
-  var startTime = date.getTime();
+  var startTime = 10; // initialize test user with 10 minutes of parking time;
   var user = new User(userPos, startTime);
 
 }
 
 function locate(locationWindow, map, parkingAreas) {
-    // Try HTML5 geolocation.
+  // Try HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
       var pos = {
@@ -154,9 +154,9 @@ function locate(locationWindow, map, parkingAreas) {
       map.setCenter(pos);
 
       parkingAreas.forEach(function (element) {
-         if (google.maps.geometry.poly.containsLocation(new google.maps.LatLng(pos.lat, pos.lng), element.polygon)) {
-            locationWindow.setContent("You are inside a TrePark area. Park here to earn minutes");
-            return;
+        if (google.maps.geometry.poly.containsLocation(new google.maps.LatLng(pos.lat, pos.lng), element.polygon)) {
+          locationWindow.setContent("You are inside a TrePark area. Park here to earn minutes");
+          return;
         }
       });
     }, function () {
@@ -178,10 +178,18 @@ function locate(locationWindow, map, parkingAreas) {
 
 function park(parkArea) {
   parkArea.parkHere();
+  user.setParked(true);
+  return new ParkingEvent(user, d.getTime(), parkArea);
 }
 
 function leave(parkArea) {
   parkArea.leave();
+}
+
+function leave(parkArea, parkingEvent) {
+  parkArea.leave();
+  user.setParked(false);
+  user.setTime(parkingEvent.getDuration);
 }
 
 function initialize(parkArea, map, infowindow) {
--- user.js
+++ user.js
@@ -1,6 +1,9 @@
 function User(pos, time) {
+    // pos = lat, lng
+    // time = minutes left on account
     this.time = time;
     this.pos = pos;
+    this.parked = false;
 
     this.setTime(time) = function() {
         this.time = time;
@@ -10,11 +13,24 @@ function User(pos, time) {
         this.pos = pos;
     }
 
+    this.setPos(lat, lng) = function() {
+        this.pos.lat = lat;
+        this.pos.lng = lng;
+    }
+
+    this.setParked(parked) = function() {
+        this.parked = parked;
+    }
+
     this.getTime() = function() {
         return this.time;
     }
 
     this.getPos() = function() {
         return this.pos;
+    }
+
+    this.getParked() = function() {
+        return this.parked;
     }
 }
